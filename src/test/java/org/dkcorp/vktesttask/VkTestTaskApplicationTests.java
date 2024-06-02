46f47a8daf1222eec93a117ced922b02ea4c79c7
Denis Karpov <karpovdenis.al@gmail.com>
2024-06-02 13:12:16 +0300

    Init project

---
+++ src/test/java/org/dkcorp/vktesttask/VkTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dkcorp.vktesttask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VkTestTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
