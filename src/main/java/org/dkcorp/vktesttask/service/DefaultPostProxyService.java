163de7e91c15fcc20fbd5af51b20a954c285dbb0
Denis Karpov <karpovdenis.al@gmail.com>
2024-06-02 16:38:23 +0300

    Add default realization for posts proxy service

---
+++ src/main/java/org/dkcorp/vktesttask/service/DefaultPostProxyService.java
@@ -0,0 +1,43 @@
+package org.dkcorp.vktesttask.service;
+
+import lombok.RequiredArgsConstructor;
+import org.dkcorp.vktesttask.dto.CommentDto;
+import org.dkcorp.vktesttask.dto.PostDto;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class DefaultPostProxyService implements PostsProxyService {
+    private final WebClient webClient;
+
+    @Override
+    public List<PostDto> getAllPosts() {
+        return webClient.get()
+                .uri("/posts")
+                .retrieve().bodyToFlux(PostDto.class)
+                .collectList()
+                .block();
+    }
+
+    @Override
+    public PostDto getPostById(Long id) {
+        return webClient.get()
+                .uri("/posts/" + id)
+                .retrieve()
+                .bodyToMono(PostDto.class)
+                .block();
+    }
+
+    @Override
+    public List<CommentDto> getPostComments(Long id) {
+        return webClient.get()
+                .uri("/posts/" + id + "/comments").
+                retrieve()
+                .bodyToFlux(CommentDto.class)
+                .collectList()
+                .block();
+    }
+}
