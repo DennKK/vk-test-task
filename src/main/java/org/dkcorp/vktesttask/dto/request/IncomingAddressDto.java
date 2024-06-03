fed23fee76e35e03890fdc7be153e858ff3f4769
Denis Karpov <karpovdenis.al@gmail.com>
2024-06-03 15:47:43 +0300

    Refactor to use incoming geo dto

--- src/main/java/org/dkcorp/vktesttask/dto/request/IncomingAddressDto.java
+++ src/main/java/org/dkcorp/vktesttask/dto/request/IncomingAddressDto.java
@@ -1,12 +1,10 @@
 package org.dkcorp.vktesttask.dto.request;
 
-import org.dkcorp.vktesttask.dto.response.GeoDto;
-
 public record IncomingAddressDto(
         String street,
         String suite,
         String city,
         String zipcode,
-        GeoDto geo
+        IncomingGeoDto geo
 ) {
 }
