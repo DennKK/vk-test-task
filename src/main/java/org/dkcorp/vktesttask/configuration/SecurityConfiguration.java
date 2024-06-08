83e6e5f4087d7d097c871e4b2b8f1e39f650469e
Denis Karpov <karpovdenis.al@gmail.com>
2024-06-08 15:48:40 +0300

    Add password encoder bean

--- src/main/java/org/dkcorp/vktesttask/configuration/SecurityConfiguration.java
+++ src/main/java/org/dkcorp/vktesttask/configuration/SecurityConfiguration.java
@@ -9,6 +9,8 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -29,6 +31,11 @@ public class SecurityConfiguration {
                 )
                 .httpBasic(Customizer.withDefaults())
                 .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
     @Bean
