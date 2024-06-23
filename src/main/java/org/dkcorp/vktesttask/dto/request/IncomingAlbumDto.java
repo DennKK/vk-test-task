591feb8958773e125715bdf567cfcddf3b5fc2ca
Denis Karpov <karpovdenis.al@gmail.com>
2024-06-23 18:51:23 +0300

    Remove redundant userId field from incoming album dto

--- src/main/java/org/dkcorp/vktesttask/dto/request/IncomingAlbumDto.java
+++ src/main/java/org/dkcorp/vktesttask/dto/request/IncomingAlbumDto.java
@@ -3,9 +3,6 @@ package org.dkcorp.vktesttask.dto.request;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 public record IncomingAlbumDto(
-        @Schema(description = "ID of the user who owns the album", example = "1")
-        Long userId,
-
         @Schema(description = "Title of the album", example = "Vacation Photos")
         String title
 ) {
